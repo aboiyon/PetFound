8cbb998cc88eab6d3318c2c898c82207a18f1207
Kiprotich Michael <aboiyonseronik@gmail.com>
2022-02-26 12:54:56 +0300

    Initial commit

--- app/src/main/java/com/moringa/petfinder/MainActivity.java
+++ app/src/main/java/com/moringa/petfinder/MainActivity.java
@@ -3,6 +3,7 @@ package com.moringa.petfinder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
