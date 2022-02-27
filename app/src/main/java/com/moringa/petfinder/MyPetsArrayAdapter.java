c26e8ef28a9c7bfd2ed292df291b61780eb655bf
Kiprotich Michael <aboiyonseronik@gmail.com>
2022-02-27 13:02:15 +0300

    adding a constructor to array adpater

--- app/src/main/java/com/moringa/petfinder/MyPetsArrayAdapter.java
+++ app/src/main/java/com/moringa/petfinder/MyPetsArrayAdapter.java
@@ -3,8 +3,20 @@ package com.moringa.petfinder;
 import android.content.Context;
 import android.widget.ArrayAdapter;
 
+import androidx.annotation.NonNull;
+
 public class MyPetsArrayAdapter extends ArrayAdapter {
-    private Context mContext;
-    private String[] mPets;
-    private String[] mBreeds;
+
+    private final String[] mPets;
+    private final String[] mBreeds;
+    private final String[] mSex;
+    private final String[] mLocation;
+
+    public MyPetsArrayAdapter(@NonNull Context context, int resource, String[] mPets, String[]mBreeds, String [] mSex, String[] mLocation) {
+        super(context, resource);
+        this.mPets = mPets;
+        this.mBreeds = mBreeds;
+        this.mSex = mSex;
+        this.mLocation = mLocation;
+    }
 }
