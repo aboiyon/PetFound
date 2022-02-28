18da4f54cf34e4498cfe99f27ae74f52d5c4c456
Kiprotich Michael <aboiyonseronik@gmail.com>
2022-02-28 12:08:07 +0300

    updating butterknife detais in app to refactor

--- app/src/test/java/com/moringa/petfinder/MainActivityTest.java
+++ app/src/test/java/com/moringa/petfinder/MainActivityTest.java
@@ -2,6 +2,39 @@ package com.moringa.petfinder;
 
 import static org.junit.Assert.*;
 
-public class MainActivityTest {
+import android.content.Intent;
+import android.widget.TextView;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.shadows.ShadowActivity;
+
+public class MainActivityTest {
+    private MainActivity activity;
+
+    @Before
+    public void setUp() throws Exception {
+        activity = Robolectric.buildActivity(MainActivity.class)
+                .create()
+                .resume()
+                .get();
+    }
+
+    @Test
+    public void validateTextViewContent(){
+        TextView appNameTextView = activity.findViewById(R.id.appNameTextView);
+        assertTrue("MyRestaurants".equals(appNameTextView.getText().toString()));
+    }
+
+    @Test
+    public void secondActivityStarted(){
+        activity.findViewById(R.id.findPetsButton).performClick();
+        Intent expectedIntent = new Intent(activity, PetsActivity.class);
+        ShadowActivity shadowActivity = org.robolectric.Shadows.shadowOf(activity);
+        Intent actualIntent = shadowActivity.getNextStartedActivity();
+        assertTrue(actualIntent.filterEquals(expectedIntent));
+    }
 }
