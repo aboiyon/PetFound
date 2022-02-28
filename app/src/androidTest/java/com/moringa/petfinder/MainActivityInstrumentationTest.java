b9f3d7174baea78d1086815ecad59d39979af2fb
Kiprotich Michael <aboiyonseronik@gmail.com>
2022-02-28 10:02:54 +0300

    adding espresso test updates

---
+++ app/src/androidTest/java/com/moringa/petfinder/MainActivityInstrumentationTest.java
@@ -0,0 +1,42 @@
+package com.moringa.petfinder;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+public class MainActivityInstrumentationTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> activityRule =
+            new ActivityScenarioRule<>(MainActivity.class);
+
+    @Test
+    public void validateEditText() {
+        onView(withId(R.id.locationEditText)).perform(typeText("Nairobi"))
+                .check(matches(withText("Nairobi")));
+    }
+
+    @Test
+    public void locationIsSentToPetsActivity() {
+        String location = "Nairobi";
+        onView(withId(R.id.locationEditText)).perform(typeText(location)).perform(closeSoftKeyboard());
+        try {                             // the sleep method requires to be checked and handled so we use try block
+            Thread.sleep(250);
+        } catch (InterruptedException e){
+            System.out.println("got interrupted!");
+        }
+        onView(withId(R.id.findPetsButton)).perform(click());
+        onView(withId(R.id.locationTextView)).check(matches
+                (withText("Hybrid pets: " + location)));
+    }
+
+}
---
+++ app/src/test/java/com/moringa/petfinder/MainActivityTest.java
@@ -0,0 +1,7 @@
+package com.moringa.petfinder;
+
+import static org.junit.Assert.*;
+
+public class MainActivityTest {
+
+}
