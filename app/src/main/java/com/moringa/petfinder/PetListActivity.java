e6dd57552f441927b2fef6e74f28d6371db18d9c
Kiprotich Michael <aboiyonseronik@gmail.com>
2022-03-01 20:49:35 +0300

    added a list class for displaying pets

--- app/src/main/java/com/moringa/petfinder/PetListActivity.java
+++ app/src/main/java/com/moringa/petfinder/PetListActivity.java
@@ -3,22 +3,31 @@ package com.moringa.petfinder;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.moringa.petfinder.adapters.PetListAdapter;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class PetListActivity extends AppCompatActivity {
-    private String [] pets = new String[]{"Rex","Simba","Mata","T.z","Ceyos","Hesky","Bosco","South","Devs","Sparta","Java","Maven","Mambuzi","Rexxie","Ketepa"};
-    private String [] breeds = new String[] {"Beagle", "Pomerarian", "Siberian Husky", "Scandinavian", "Bichon Frise", "Bichon King", "Maltese Dog", "Bulldog", "Labrador Retriever", "Mexican", "Basset Hound", "Cuban", "Pug", "Shiba Inu", "Bull Terrier" };
-    private String [] sex = new String[] {"Male","Female","Male","Female","Female","Male","Female","Male","Female","Female","Male","Female","Male","Female","Female"};
-    private String [] location = new String[] {"Kyiv","Kharkov","Siberia","Alps","Atlas","Pampas","Downs","Veld","Tigers","East","West","Mombasa","Nairobi","Kisumu","Kampala","Lebekwet"};
-    @BindView(R.id.locationTextView) TextView mLocationTextView;
-    @BindView(R.id.recyclerView) ListView mListView;
+    private static final String TAG = PetListActivity.class.getSimpleName();
+
+    @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
+    @BindView(R.id.errorTextView) TextView mErrorTextView;
+    @BindView(R.id.progressBar) ProgressBar mProgressBar;
+
+    private PetListAdapter mAdapter;
+
+    public List<Business> restaurants;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,19 +35,58 @@ public class PetListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pets);
         ButterKnife.bind(this);
 
-        MyPetsArrayAdapter adapter = new MyPetsArrayAdapter(this, android.R.layout.simple_expandable_list_item_1, pets,breeds,sex,location); // the arguments must match constructor's parameters!
-        mListView.setAdapter(adapter);
-
-        mListView.setOnItemClickListener((adapterView, view, position, l) -> {
-            String pet = ((TextView)view).getText().toString();
-            Log.v("PetsActivity", "In the onItemClickListener!");
-            Toast.makeText(PetListActivity.this, pet, Toast.LENGTH_LONG).show();
-        });
-
         Intent intent = getIntent();
         String location = intent.getStringExtra("location");
-        mLocationTextView.setText(String.format(String.format("Popular pets:%s", location)));
-        Log.d("PetsActivity", "In the onCreate method!");
+
+        YelpApi client = YelpClient.getClient();
+
+        Call<YelpBusinessesSearchResponse> call = client.getPets(location, "pets");
+
+        call.enqueue(new Callback<YelpBusinessesSearchResponse>() {
+            @Override
+            public void onResponse(Call<YelpBusinessesSearchResponse> call, Response<YelpBusinessesSearchResponse> response) {
+                hideProgressBar();
+
+                if (response.isSuccessful()) {
+                    pets = response.body().getBusinesses();
+                    mAdapter = new PetListAdapter(PetListActivity.this, pets);
+                    mRecyclerView.setAdapter(mAdapter);
+                    RecyclerView.LayoutManager layoutManager =
+                            new LinearLayoutManager(PetListActivity.this);
+                    mRecyclerView.setLayoutManager(layoutManager);
+                    mRecyclerView.setHasFixedSize(true);
+
+                    showPets();
+                } else {
+                    showUnsuccessfulMessage();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<YelpBusinessesSearchResponse> call, Throwable t) {
+                hideProgressBar();
+                showFailureMessage();
+            }
+
+        });
+    }
+
+    private void showFailureMessage() {
+        mErrorTextView.setText("Something went wrong. Please check your Internet connection and try again later");
+        mErrorTextView.setVisibility(View.VISIBLE);
+    }
+
+    private void showUnsuccessfulMessage() {
+        mErrorTextView.setText("Something went wrong. Please try again later");
+        mErrorTextView.setVisibility(View.VISIBLE);
+    }
+
+    private void showRestaurants() {
+        mRecyclerView.setVisibility(View.VISIBLE);
+    }
+
+    private void hideProgressBar() {
+        mProgressBar.setVisibility(View.GONE);
     }
 }
 
