1ac68cdd8d04e56d366f9b4612a3470df6018a92
Kiprotich Michael <aboiyonseronik@gmail.com>
2022-03-02 07:05:56 +0300

    updated main activity

--- app/src/main/java/com/moringa/petfinder/PetListActivity.java
+++ app/src/main/java/com/moringa/petfinder/PetListActivity.java
@@ -1,96 +1,96 @@
-//package com.moringa.petfinder;
-//
-//import android.content.Intent;
-//import android.os.Bundle;
-//import android.util.Log;
-//import android.view.View;
-//import android.widget.ListView;
-//import android.widget.ProgressBar;
-//import android.widget.TextView;
-//import android.widget.Toast;
-//
-//import androidx.appcompat.app.AppCompatActivity;
-//import androidx.recyclerview.widget.LinearLayoutManager;
-//import androidx.recyclerview.widget.RecyclerView;
-//
-//import com.moringa.petfinder.adapters.PetListAdapter;
-//
-//import butterknife.BindView;
-//import butterknife.ButterKnife;
-//
-//public class PetListActivity extends AppCompatActivity {
-//    private static final String TAG = PetListActivity.class.getSimpleName();
-//
-//    @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
-//    @BindView(R.id.errorTextView) TextView mErrorTextView;
-//    @BindView(R.id.progressBar) ProgressBar mProgressBar;
-//
-//    private PetListAdapter mAdapter;
-//
-//    public List<Business> restaurants;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_pets);
-//        ButterKnife.bind(this);
-//
-//        Intent intent = getIntent();
-//        String location = intent.getStringExtra("location");
-//
-//        Api client = Client.getClient();
-//
-//        Call<YelpBusinessesSearchResponse> call = client.getPets(location, "pets");
-//
-//        call.enqueue(new Callback<YelpBusinessesSearchResponse>() {
-//            @Override
-//            public void onResponse(Call<YelpBusinessesSearchResponse> call, Response<YelpBusinessesSearchResponse> response) {
-//                hideProgressBar();
-//
-//                if (response.isSuccessful()) {
-//                    pets = response.body().getBusinesses();
-//                    mAdapter = new PetListAdapter(PetListActivity.this, pets);
-//                    mRecyclerView.setAdapter(mAdapter);
-//                    RecyclerView.LayoutManager layoutManager =
-//                            new LinearLayoutManager(PetListActivity.this);
-//                    mRecyclerView.setLayoutManager(layoutManager);
-//                    mRecyclerView.setHasFixedSize(true);
-//
-//                    showPets();
-//                } else {
-//                    showUnsuccessfulMessage();
-//                }
-//            }
-//
-//            @Override
-//            public void onFailure(Call<YelpBusinessesSearchResponse> call, Throwable t) {
-//                hideProgressBar();
-//                showFailureMessage();
-//            }
-//
-//        });
-//    }
-//
-//    private void showFailureMessage() {
-//        mErrorTextView.setText("Something went wrong. Please check your Internet connection and try again later");
-//        mErrorTextView.setVisibility(View.VISIBLE);
-//    }
-//
-//    private void showUnsuccessfulMessage() {
-//        mErrorTextView.setText("Something went wrong. Please try again later");
-//        mErrorTextView.setVisibility(View.VISIBLE);
-//    }
-//
-//    private void showRestaurants() {
-//        mRecyclerView.setVisibility(View.VISIBLE);
-//    }
-//
-//    private void hideProgressBar() {
-//        mProgressBar.setVisibility(View.GONE);
-//    }
-//}
-//
-//
-//
-////API-KEY:bKYfebFDtZTVNWAbv7BMsmQYIgRAlYW717TqhFQWW4ZgEW3gQe
-////SECRET-KEY: YRKwMSMd55uY25rxh4xOSssyZO1mu7uQNEoCR5rw
+package com.moringa.petfinder;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.moringa.petfinder.adapters.PetListAdapter;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class PetListActivity extends AppCompatActivity {
+    private static final String TAG = PetListActivity.class.getSimpleName();
+
+    @BindView(R.id.recyclerView) RecyclerView mRecyclerView;
+    @BindView(R.id.errorTextView) TextView mErrorTextView;
+    @BindView(R.id.progressBar) ProgressBar mProgressBar;
+
+    private PetListAdapter mAdapter;
+
+    public List<Business> restaurants;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pets);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+
+        Api client = Client.getClient();
+
+        Call<YelpBusinessesSearchResponse> call = client.getPets(location, "pets");
+
+        call.enqueue(new Callback<YelpBusinessesSearchResponse>() {
+            @Override
+            public void onResponse(Call<YelpBusinessesSearchResponse> call, Response<YelpBusinessesSearchResponse> response) {
+                hideProgressBar();
+
+                if (response.isSuccessful()) {
+                    pets = response.body().getBusinesses();
+                    mAdapter = new PetListAdapter(PetListActivity.this, pets);
+                    mRecyclerView.setAdapter(mAdapter);
+                    RecyclerView.LayoutManager layoutManager =
+                            new LinearLayoutManager(PetListActivity.this);
+                    mRecyclerView.setLayoutManager(layoutManager);
+                    mRecyclerView.setHasFixedSize(true);
+
+                    showPets();
+                } else {
+                    showUnsuccessfulMessage();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<YelpBusinessesSearchResponse> call, Throwable t) {
+                hideProgressBar();
+                showFailureMessage();
+            }
+
+        });
+    }
+
+    private void showFailureMessage() {
+        mErrorTextView.setText("Something went wrong. Please check your Internet connection and try again later");
+        mErrorTextView.setVisibility(View.VISIBLE);
+    }
+
+    private void showUnsuccessfulMessage() {
+        mErrorTextView.setText("Something went wrong. Please try again later");
+        mErrorTextView.setVisibility(View.VISIBLE);
+    }
+
+    private void showRestaurants() {
+        mRecyclerView.setVisibility(View.VISIBLE);
+    }
+
+    private void hideProgressBar() {
+        mProgressBar.setVisibility(View.GONE);
+    }
+}
+
+
+
+//API-KEY:bKYfebFDtZTVNWAbv7BMsmQYIgRAlYW717TqhFQWW4ZgEW3gQe
+//SECRET-KEY: YRKwMSMd55uY25rxh4xOSssyZO1mu7uQNEoCR5rw
