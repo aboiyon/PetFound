349b58ee56858a3f4ae0345fcb024ed885850be1
Kiprotich Michael <aboiyonseronik@gmail.com>
2022-03-03 21:59:06 +0300

    update get method by deleting search extension

--- app/src/main/java/com/moringa/petfinder/network/interface1.java
+++ app/src/main/java/com/moringa/petfinder/network/interface1.java
@@ -7,7 +7,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface interface1 {
-    @GET("animals/search")
+    @GET("animals")
     Call<SearchResponse> getPets(
             @Query("type") String type
     );
