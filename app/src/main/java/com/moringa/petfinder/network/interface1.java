3e8b21dbff94821446b373a748d9b074e67d1e0b
Kiprotich Michael <aboiyonseronik@gmail.com>
2022-03-06 12:21:41 +0300

    added get request

--- app/src/main/java/com/moringa/petfinder/network/interface1.java
+++ app/src/main/java/com/moringa/petfinder/network/interface1.java
@@ -7,7 +7,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface interface1 {
-    @GET("animals")
+    @GET("animals?type=dog&page=2")
     Call<SearchResponse> getPets(
             @Query("type") String type
     );
