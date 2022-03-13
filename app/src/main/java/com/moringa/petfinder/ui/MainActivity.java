bcbcc6c9da73242051732daf14a3349adce20a7d
Kiprotich Michael <aboiyonseronik@gmail.com>
2022-03-13 22:08:18 +0300

    added text button

--- app/src/main/java/com/moringa/petfinder/ui/MainActivity.java
+++ app/src/main/java/com/moringa/petfinder/ui/MainActivity.java
@@ -4,7 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,14 +17,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 //    private ValueEventListener mSearchedPetReferenceListener;
 //    private DatabaseReference mSearchedPetReference;
 
-    @BindView(R.id.findPetsButton)
-    Button mFindPetsButton;
-    @BindView(R.id.locationEditText)
-    EditText mLocationEditText;
-    @BindView(R.id.appNameTextView)
-    TextView mAppNameTextView;
-    @BindView(R.id.savedPetsButton)
-    Button mSavedPetsButton;
+    @BindView(R.id.findPetsButton) Button mFindPetsButton;
+//    @BindView(R.id.locationEditText) EditText mLocationEditText;
+    @BindView(R.id.appNameTextView) TextView mAppNameTextView;
+    @BindView(R.id.savedPetsButton) Button mSavedPetsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
