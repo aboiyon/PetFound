d81b3a9165ff0521642262474a2776818be720b1
Kiprotich Michael <aboiyonseronik@gmail.com>
2022-03-13 22:48:12 +0300

    added a registration form

---
+++ app/src/main/java/com/moringa/petfinder/ui/CreateAccountActivity.java
@@ -0,0 +1,16 @@
+package com.moringa.petfinder.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.moringa.petfinder.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
