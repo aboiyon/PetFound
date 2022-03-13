7f83dc9f94ba1d45fcc33398fa327e4f006fe72e
Kiprotich Michael <aboiyonseronik@gmail.com>
2022-03-14 01:45:34 +0300

    added an update to logout

--- app/src/main/java/com/moringa/petfinder/ui/MainActivity.java
+++ app/src/main/java/com/moringa/petfinder/ui/MainActivity.java
@@ -2,23 +2,24 @@ package com.moringa.petfinder.ui;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.moringa.petfinder.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-//    private ValueEventListener mSearchedPetReferenceListener;
-//    private DatabaseReference mSearchedPetReference;
 
     @BindView(R.id.findPetsButton) Button mFindPetsButton;
-//    @BindView(R.id.locationEditText) EditText mLocationEditText;
     @BindView(R.id.appNameTextView) TextView mAppNameTextView;
     @BindView(R.id.savedPetsButton) Button mSavedPetsButton;
 
@@ -33,69 +34,40 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         mSavedPetsButton.setOnClickListener(this);
     }
     @Override
-    public void onClick(View v) {
+    public void onClick(View view) {
 
-        if(v == mFindPetsButton) {
+        if (view == mFindPetsButton) {
             Intent intent = new Intent(MainActivity.this, PetListActivity.class);
             startActivity(intent);
         }
 
-        if (v == mSavedPetsButton) {
+        if (view == mSavedPetsButton) {
             Intent intent = new Intent(MainActivity.this, SavedPetListActivity.class);
             startActivity(intent);
         }
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(intent);
+        finish();
+    }
 }
-//        mSearchedPetReference = FirebaseDatabase
-//                .getInstance()
-//                .getReference()
-//                .child(Constants.FIREBASE_CHILD_SEARCHED_PET);
-//
-//
-//        mSearchedPetReferenceListener = mSearchedPetReference.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                for(DataSnapshot typeSnapshot : dataSnapshot.getChildren()){
-//                    String type = typeSnapshot.getValue().toString();
-//                    Log.d("Pet updated", "type: " + type);
-//                }
-//            }
-//
-//            @Override
-//            public void onCancelled(@NonNull DatabaseError databaseError) {
-//
-//            }
-//        });
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-//        ButterKnife.bind(this);
-//
-//        mFindPetsButton.setOnClickListener(this);
-//        mSavedPetsButton.setOnClickListener(this);
-//    }
-//
-//    @Override
-//    public void onClick(View view){
-//        if (view == mFindPetsButton) {
-//            String type = mLocationEditText.getText().toString();
-//
-//            savePetToFirebase(type);
-//            Intent intent = new Intent(MainActivity.this, PetListActivity.class);
-//            intent.putExtra("type", type);
-//            startActivity(intent);
-//        }
-//        if (view == mSavedPetsButton) {
-//            Intent intent = new Intent(MainActivity.this, SavedPetListActivity.class);
-//            startActivity(intent);
-//        }
-//    }
-//
-//    public void savePetToFirebase(String location) {
-//        mSearchedPetReference.push().setValue(location);
-//    }
-//    @Override
-//    protected void onDestroy() {
-//        super.onDestroy();
-//        mSearchedPetReference.removeEventListener(mSearchedPetReferenceListener);
-//    }
-//}
+
+
