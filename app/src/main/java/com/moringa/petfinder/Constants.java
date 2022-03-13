1260d2eb13111641d34e35da3ee67f4abf9b64c9
Kiprotich Michael <aboiyonseronik@gmail.com>
2022-03-13 21:33:07 +0300

    refactored petlist to include shared preferences

--- app/src/main/java/com/moringa/petfinder/Constants.java
+++ app/src/main/java/com/moringa/petfinder/Constants.java
@@ -3,6 +3,7 @@ package com.moringa.petfinder;
 public class Constants {
     public static final String BASE_URL= "https://api.petfinder.com/v2/";
     public static final String access_token = BuildConfig.access_token;
+    public static final String PREFERENCES_TYPE_KEY = "type";
     public static final String FIREBASE_CHILD_SEARCHED_PET = "searchedPet";
     public static final String FIREBASE_CHILD_PETS = "pets";
 }
--- app/src/main/java/com/moringa/petfinder/ui/PetListActivity.java
+++ app/src/main/java/com/moringa/petfinder/ui/PetListActivity.java
@@ -1,17 +1,23 @@
 package com.moringa.petfinder.ui;
 
 
-import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.SearchView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.moringa.petfinder.Constants;
 import com.moringa.petfinder.R;
 import com.moringa.petfinder.adapters.PetListAdapter;
 import com.moringa.petfinder.models.Animal;
@@ -28,6 +34,9 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class PetListActivity extends AppCompatActivity {
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+    private String mRecentAddress;
     private static final String TAG = PetListActivity.class.getSimpleName();
     private PetListAdapter mAdapter;
 
@@ -42,38 +51,46 @@ public class PetListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pets);
         ButterKnife.bind(this);
 
-        Intent intent = getIntent();
-        String type = intent.getStringExtra("type");
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mRecentAddress = mSharedPreferences.getString(Constants.PREFERENCES_TYPE_KEY, null);
+        if (mRecentAddress != null) {
+            fetchPets(mRecentAddress);
+        }
+    }
 
-        PetApi client = PetClient.getClient();
-        Call<SearchResponse> call = client.getPets(type);
-        call.enqueue(new Callback<SearchResponse>() {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_search, menu);
+        ButterKnife.bind(this);
+
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        mEditor = mSharedPreferences.edit();
+
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public void onResponse(Call<SearchResponse> call, Response<SearchResponse> response) {
-                hideProgressBar();
-
-                if (response.isSuccessful()) {
-                    pets = response.body().getAnimals();
-                    mAdapter = new PetListAdapter(PetListActivity.this, pets);
-                    mRecyclerView.setAdapter(mAdapter);
-                    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(PetListActivity.this);
-                    mRecyclerView.setLayoutManager(layoutManager);
-                    mRecyclerView.setHasFixedSize(true);
-
-                    showPets();
-                } else {
-                    showUnsuccessfulMessage();
-                }
+            public boolean onQueryTextSubmit(String type) {
+                addToSharedPreferences(type);
+                fetchPets(type);
+                return false;
             }
 
-            @Override
-            public void onFailure(Call<SearchResponse> call, Throwable t) {
-                Log.e(TAG, "onFailure: ",t );
-                hideProgressBar();
-                showFailureMessage();
-            }
 
+            @Override
+            public boolean onQueryTextChange(String type) {
+                return false;
+            }
         });
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 
     private void showFailureMessage() {
@@ -86,11 +103,101 @@ public class PetListActivity extends AppCompatActivity {
         mErrorTextView.setVisibility(View.VISIBLE);
     }
 
-    private void showPets() {
+    private void showRestaurants() {
         mRecyclerView.setVisibility(View.VISIBLE);
     }
 
     private void hideProgressBar() {
         mProgressBar.setVisibility(View.GONE);
     }
+
+    private void addToSharedPreferences(String type) {
+        mEditor.putString(Constants.PREFERENCES_TYPE_KEY, type).apply();
+    }
+
+    private void fetchPets(String type) {
+        PetApi client = PetClient.getClient();
+        Call<SearchResponse> call = client.getPets(type);
+        call.enqueue(new Callback<SearchResponse>() {
+            @Override
+            public void onResponse(Call<SearchResponse> call, Response<SearchResponse> response) {
+
+                hideProgressBar();
+
+                if (response.isSuccessful()) {
+                    pets = response.body().getAnimals();
+                    mAdapter = new PetListAdapter(PetListActivity.this, pets);
+                    mRecyclerView.setAdapter(mAdapter);
+                    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(PetListActivity.this);
+                    mRecyclerView.setLayoutManager(layoutManager);
+                    mRecyclerView.setHasFixedSize(true);
+
+                    showRestaurants();
+                } else {
+                    showUnsuccessfulMessage();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<SearchResponse> call, Throwable t) {
+                Log.e(TAG, "onFailure: ", t);
+                hideProgressBar();
+                showFailureMessage();
+            }
+
+        });
+    }
 }
+
+//        Intent intent = getIntent();
+//        String type = intent.getStringExtra("type");
+//
+//        PetApi client = PetClient.getClient();
+//        Call<SearchResponse> call = client.getPets(type);
+//        call.enqueue(new Callback<SearchResponse>() {
+//            @Override
+//            public void onResponse(Call<SearchResponse> call, Response<SearchResponse> response) {
+//                hideProgressBar();
+//
+//                if (response.isSuccessful()) {
+//                    pets = response.body().getAnimals();
+//                    mAdapter = new PetListAdapter(PetListActivity.this, pets);
+//                    mRecyclerView.setAdapter(mAdapter);
+//                    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(PetListActivity.this);
+//                    mRecyclerView.setLayoutManager(layoutManager);
+//                    mRecyclerView.setHasFixedSize(true);
+//
+//                    showPets();
+//                } else {
+//                    showUnsuccessfulMessage();
+//                }
+//            }
+//
+//            @Override
+//            public void onFailure(Call<SearchResponse> call, Throwable t) {
+//                Log.e(TAG, "onFailure: ",t );
+//                hideProgressBar();
+//                showFailureMessage();
+//            }
+//
+//        });
+//    }
+//
+//    private void showFailureMessage() {
+//        mErrorTextView.setText("Something went wrong. Please check your Internet connection and try again later");
+//        mErrorTextView.setVisibility(View.VISIBLE);
+//    }
+//
+//    private void showUnsuccessfulMessage() {
+//        mErrorTextView.setText("Something went wrong. Please try again later");
+//        mErrorTextView.setVisibility(View.VISIBLE);
+//    }
+//
+//    private void showPets() {
+//        mRecyclerView.setVisibility(View.VISIBLE);
+//    }
+//
+//    private void hideProgressBar() {
+//        mProgressBar.setVisibility(View.GONE);
+//    }
+//}
