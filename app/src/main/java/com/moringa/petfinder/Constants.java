ae3df19d8c351c9f1df32eea736117d2405497d3
Kiprotich Michael <aboiyonseronik@gmail.com>
2022-03-13 10:27:53 +0300

    added restaurant node key name

--- app/src/main/java/com/moringa/petfinder/Constants.java
+++ app/src/main/java/com/moringa/petfinder/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
     public static final String BASE_URL= "https://api.petfinder.com/v2/";
     public static final String access_token = BuildConfig.access_token;
     public static final String FIREBASE_CHILD_SEARCHED_PET = "searchedPet";
+    public static final String FIREBASE_CHILD_PETS = "pets";
 }
