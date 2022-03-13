f3134993ad511920a0d372765153bd757ef73ea9
Kiprotich Michael <aboiyonseronik@gmail.com>
2022-03-13 09:43:11 +0300

    added search pet node

--- app/src/main/java/com/moringa/petfinder/Constants.java
+++ app/src/main/java/com/moringa/petfinder/Constants.java
@@ -2,5 +2,6 @@ package com.moringa.petfinder;
 
 public class Constants {
     public static final String BASE_URL= "https://api.petfinder.com/v2/";
-    public static final String access_token = BuildConfig.API_KEY;
+    public static final String access_token = BuildConfig.access_token;
+    public static final String FIREBASE_CHILD_SEARCHED_PET = "searchedPet";
 }
