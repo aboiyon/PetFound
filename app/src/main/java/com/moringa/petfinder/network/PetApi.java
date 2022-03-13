3f3ca9eb6fd16ad05f36811f37b832fb4b8edf32
Kiprotich Michael <aboiyonseronik@gmail.com>
2022-03-13 19:35:38 +0300

    updated the interface

--- app/src/main/java/com/moringa/petfinder/network/PetApi.java
+++ app/src/main/java/com/moringa/petfinder/network/PetApi.java
@@ -4,12 +4,13 @@ import com.moringa.petfinder.models.SearchResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 public interface PetApi {
 //    @Headers({"Accept:application/vnd.\"Bearer bKYfebFDtZTVNWAbv7BMsmQYIgRAlYW717TqhFQWW4ZgEW3gQe\".v1.full+json",
 //    "User-Agent:PetFinder"})
     @GET("Animals/types")
     Call<SearchResponse> getPets(
-//            @Query("type") String type
+            @Query("type") String type
     );
 }
