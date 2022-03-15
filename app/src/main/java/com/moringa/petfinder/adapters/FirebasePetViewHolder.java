d497331a44b4128ebaac1c5063ea5b42a12105f7
Kiprotich Michael <aboiyonseronik@gmail.com>
2022-03-15 08:54:54 +0300

    added database reference userid

--- app/src/main/java/com/moringa/petfinder/adapters/FirebasePetViewHolder.java
+++ app/src/main/java/com/moringa/petfinder/adapters/FirebasePetViewHolder.java
@@ -8,6 +8,8 @@ import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -48,7 +50,9 @@ public class FirebasePetViewHolder extends RecyclerView.ViewHolder implements Vi
     @Override
     public void onClick(View view) {
         final ArrayList<Animal>pets = new ArrayList<>();
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_PETS);
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String uid = user.getUid();
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_PETS).child(uid);
         ref.addListenerForSingleValueEvent(new ValueEventListener() {
 
             @Override
