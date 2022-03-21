15bc1c87cc7784016a36f3ae4c2e608192424af3
Kiprotich Michael <aboiyonseronik@gmail.com>
2022-03-21 08:54:32 +0300

    added index used to order a list of pets from  firebase

---
+++ app/src/main/java/com/moringa/petfinder/models/Pet.java
@@ -0,0 +1,79 @@
+package com.moringa.petfinder.models;
+
+import org.parceler.Parcel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Parcel
+public class Pet {
+    String name;
+    String phone;
+    String website;
+    double rating;
+    String imageUrl;
+    List<String> address = new ArrayList<>();
+    double latitude;
+    double longitude;
+    List<String> categories = new ArrayList<>();
+    private String pushId;
+    String index;
+
+    public Pet(){}
+
+    public Pet(String name, String phone, String website, ArrayList<String> address){
+        this.name = name;
+        this.phone = phone;
+        this.website = website;
+        this.rating = rating;
+        this.imageUrl = imageUrl;
+        this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.categories = categories;
+        this.index = "not_specified";
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getWebsite() {
+        return  website;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public String getImageUrl(){
+        return imageUrl;
+    }
+
+    public List<String> getAddress() {
+        return address;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public String getPushId(){
+        return pushId;
+    }
+
+    public void setPushId(String pushId){
+        this.pushId = pushId;
+    }
+
+    public String getIndex() {
+        return index;
+    }
+
+    public void setIndex(String index) {
+        this.index = index;
+    }
+}
