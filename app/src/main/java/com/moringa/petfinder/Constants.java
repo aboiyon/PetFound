e81b492798b130e29071e54af166a860fa8f4d5f
Kiprotich Michael <aboiyonseronik@gmail.com>
2022-03-21 08:33:39 +0300

    added index key for pet object

--- app/src/main/java/com/moringa/petfinder/Constants.java
+++ app/src/main/java/com/moringa/petfinder/Constants.java
@@ -6,4 +6,5 @@ public class Constants {
     public static final String PREFERENCES_TYPE_KEY = "type";
     public static final String FIREBASE_CHILD_SEARCHED_PET = "searchedPet";
     public static final String FIREBASE_CHILD_PETS = "pets";
+    public static final String FIREBASE_QUERY_INDEX = "index";
 }
